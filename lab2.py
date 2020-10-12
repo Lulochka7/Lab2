787f03cc8c463073254f383c88aede252a2dd60e
Lulochka7 <72259459+Lulochka7@users.noreply.github.com>
2020-10-12 23:30:41 +0700

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,21 @@
+class Persona:
+    def __init__(self, name, surname, post):
+        self.name = name
+        self.surname = surname 
+        self.post = post    
+persona = Persona("Иван", "Иванов", "Программист")
+
+class Time:
+    def __init__(self, work, after, fine):
+        self.work = work
+        self.after = after
+        self.fine = fine
+time = Time(50, 3, 1)
+
+class Payment:
+    def __init__(self, oklad, premium, material):
+        self.oklad = oklad
+        self.premium = premium
+        self.material = material
+payment = Payment(400, 600, 300) 
+    print(persona.post)
